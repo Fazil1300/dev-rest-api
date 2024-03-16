67611a760929b60dae3be49c7340d86e88e9aefd
Fazil1300 <mohamedfazil1300@gmail.com>
2024-03-16 15:22:25 +0530

    JSON jackson binding

---
+++ src/main/java/com/fazil/rest/api/demo/rest/StudentController.java
@@ -0,0 +1,23 @@
+package com.fazil.rest.api.demo.rest;
+
+import com.fazil.rest.api.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class StudentController {
+    @GetMapping("/students")
+    public List<Student> studentList(){
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Taylor","Swift"));
+        theStudents.add(new Student("Zayn","Malik"));
+        theStudents.add(new Student("Justin","Bieber"));
+
+        return theStudents;
+    }
+}
