8b37244226360ba59cbdc5ef36b09c27e060e18b
Fazil1300 <mohamedfazil1300@gmail.com>
2024-03-16 15:44:12 +0530

    Refactoring code

--- src/main/java/com/fazil/rest/api/demo/rest/StudentRestController.java
+++ src/main/java/com/fazil/rest/api/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.fazil.rest.api.demo.rest;
 
 import com.fazil.rest.api.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> studentList(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
         theStudents.add(new Student("Taylor","Swift"));
         theStudents.add(new Student("Zayn","Malik"));
         theStudents.add(new Student("Justin","Bieber"));
+        theStudents.add(new Student("Ariana","Grande"));
 
+    }
+    @GetMapping("/students")
+    public List<Student> studentList(){
         return theStudents;
     }
 }
